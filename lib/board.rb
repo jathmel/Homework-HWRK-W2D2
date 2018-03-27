e3bb1e9df08558767638f1a88d5aa695136badd5
Jathmel <jathmel@gmail.com>
2018-03-27 02:17:38 -0400

    defined initialize and place_stone methods

--- lib/board.rb
+++ lib/board.rb
@@ -2,10 +2,22 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) {Array.new}
+    place_stones
+
+
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, idx|
+      next if idx == 6 || idx == 13
+      4.times do
+        cup <<  "stone"
+      end
+    end
   end
 
   def valid_move?(start_pos)
